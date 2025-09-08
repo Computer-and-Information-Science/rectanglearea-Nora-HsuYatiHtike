bd19ac80998359362b611f970921a193399852c1
Hsu Yati Htike <94952859+Nora-HsuYatiHtike@users.noreply.github.com>
2025-09-08 06:32:58 +0000

    rectangle area

--- main.cpp
+++ main.cpp
@@ -5,5 +5,15 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+// calculate area and perimeter
+double area = length * width;
+double perimeter = 2 * (length + width);
 
+// output exactly as expected
+cout << "Length = " << length << endl;
+cout << "Width = " << width << endl;
+cout << "Area = " << area << endl;
+cout << "Perimeter = " << perimeter << endl;
+
+return 0;
 }
